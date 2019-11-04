cc22ea538cbf0e5a48f2a904463bc7815d9ea191
Mark Eaton <Mark.Eaton@kbcc.cuny.edu>
2019-11-04 16:39:36 -0500

    add missing data; set ylims; add comments

--- chart.py
+++ chart.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from dateutil.parser import parse
 from matplotlib import pyplot as plt
 from pathlib import Path
-from pprint import pprint
 from textblob import TextBlob
 
 tweets1 = []
@@ -13,6 +12,7 @@ tweets2 = []
 data = []
 error_count = 0
 
+# loop through the twarc archive
 pathlist1 = Path("data-twarc/").glob("*.json")
 for path in pathlist1:
     with open(str(path)) as f1:
@@ -24,7 +24,9 @@ for path in pathlist1:
             except json.decoder.JSONDecodeError:
                 error_count += 1
                 print('Error reading json!')
+                pass
 
+# loop through the tcat archive
 pathlist2 = Path("data-tcat/").glob("*.csv")
 for path in pathlist2:
     with open(str(path), encoding="utf-8") as f2:
@@ -38,8 +40,11 @@ for path in pathlist2:
             except:
                 error_count += 1
                 print("Error reading csv!")
-                print(line[2])
-                raise
+                raise # raise because passing here would omit too many values
+
+        # two missing months; add None values 
+        data.append((datetime(2018, 1, 1), "", None))
+        data.append((datetime(2018, 2, 1), "", None))
 
 df = pd.DataFrame(data=data)
 df.columns = ["date", "tweet", "sentiment"]
@@ -47,10 +52,8 @@ df['date'] = pd.to_datetime(df['date'], infer_datetime_format=True)
 df['month'] = df['date'].dt.strftime('%Y-%m')
 grouped = df.groupby('month')['sentiment'].mean()
 grouped.columns = ["month", "mean"]
-grouped.plot(x="month", y="mean")
+grouped.plot(x="month", y="mean", ylim=(-1, 1))
 plt.show()
 
-
-print(grouped)
 print("Tweets: ", len(data))
 print("Errors: ", error_count)
