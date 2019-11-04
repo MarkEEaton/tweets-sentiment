4e26fdcb746fcf3d6ec0cfcb507a7756d2f237ec
Mark Eaton <Mark.Eaton@kbcc.cuny.edu>
2019-11-04 13:35:27 -0500

    loop through twarc files; handle and count json errors

--- chart.py
+++ chart.py
@@ -1,18 +1,29 @@
 import json
 from datetime import datetime
 from dateutil.parser import parse
+from pathlib import Path
 from pprint import pprint
 from textblob import TextBlob
 
 tweets = []
 data = []
+error_count = 0
 
-with open('data-twarc/2015.02.json') as f1:
-    for line in f1:
-        tweets.append(json.loads(line))
+pathlist = Path("data-twarc/").glob("*.json")
+for path in pathlist:
+    print(str(path))
+    with open(str(path)) as f1:
+        for line in f1:
+            try:
+                tweets.append(json.loads(line))
+            except json.decoder.JSONDecodeError:
+                error_count += 1
+                print('Error reading json!')
 
 for tweet in tweets:
     sentiment = TextBlob(tweet['text']).sentiment.polarity
     data.append((parse(tweet['created_at']), tweet['text'], sentiment))
 
 pprint(data)
+print("Tweets: ", len(data))
+print("Errors: ", error_count)
